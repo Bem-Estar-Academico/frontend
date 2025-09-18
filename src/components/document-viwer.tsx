04e2888c9c53d28c4d35d7584d7925835da4e152
Dtma20 <dtma@ic.ufal.br>
2025-09-18 17:56:17 -0300

    fix: resolves code and figma inconsistencies

--- src/components/document-viwer.tsx
+++ src/components/document-viwer.tsx
@@ -8,9 +8,9 @@ const DocumentViewer = ({ url }: DocumentViewerProps) => {
   if (!url) {
     return (
       <div className="flex flex-col h-full items-center justify-center text-gray-500 bg-gray-100 gap-[6px]">
-        <IconFileX size={100} stroke={1.25} className="text-muted-foreground"/>
-        <div className="text-lg font-medium font-inter text-[18px] text-muted-foreground">Nenhum documento selecionado</div>
-        <div className="text-base font-inter text-[14px] text-muted-foreground">
+        <IconFileX size={100} stroke={1.25} className="text-muted-foreground" />
+        <div className="font-medium font-inter text-[18px] text-muted-foreground">Nenhum documento selecionado</div>
+        <div className="font-inter text-[14px] text-muted-foreground">
           Escolha um documento da lista para visualizar
         </div>
       </div>

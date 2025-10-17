ffc476bcd3b129769453b02eec83deb1f4d04ce8
Ruan Melo <ruantmelo@gmail.com>
2025-10-17 14:26:58 -0300

    rm unused workspace file

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { resolve } from "node:path";
 
-/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["src/setup-tests.ts"],
+  },
+});

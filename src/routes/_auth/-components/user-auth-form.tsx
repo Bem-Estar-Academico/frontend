0a328b6a347279ec3a0381bd1494fbd9cd1fa527
Ruan Melo <ruantmelo@gmail.com>
2025-10-17 22:54:06 -0300

    style: improve login UI

--- src/routes/_auth/-components/user-auth-form.tsx
+++ src/routes/_auth/-components/user-auth-form.tsx
@@ -1,20 +1,47 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Link } from "@tanstack/react-router";
+import { Spinner } from "@/components/ui/spinner";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Por favor, insira um endereço de email válido.",
+  }),
+  password: z.string().min(1, {
+    message: "A senha é obrigatória.",
+  }),
+});
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     setIsLoading(true);
-    
+
     setTimeout(() => {
       setIsLoading(false);
     }, 3000);
@@ -22,38 +49,60 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={onSubmit}>
-        <div className="grid gap-4">
-          <div className="grid gap-3">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="exemplo@ufal.br"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
-            <Label className="sr-only" htmlFor="password">
-              password
-            </Label>
-            <Input
-              id="password"
-              placeholder="*********"
-              type="password"
-              autoCapitalize="none"
-              autoComplete="password"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="exemplo@ufal.br"
+                    type="email"
+                    autoCapitalize="none"
+                    autoComplete="email"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    autoCapitalize="none"
+                    autoComplete="current-password"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="grid gap-2 pt-2">
+            <Button disabled={isLoading} className="w-full">
+              {isLoading && <Spinner />}
+              Entrar
+            </Button>
+            <Button variant="link" asChild>
+              <Link to="/create-user">Crie uma conta</Link>
+            </Button>
           </div>
-          <Button disabled={isLoading}>Entrar</Button>
-          <Link to="/create-user">Crie uma conta</Link>
-        </div>
-      </form>
+        </form>
+      </Form> 
     </div>
   );
 }
--- src/routes/_auth/login.tsx
+++ src/routes/_auth/login.tsx
@@ -4,6 +4,7 @@ import { UserAuthForm } from "./-components/user-auth-form";
 import {
   Card,
   CardContent,
+  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -11,27 +12,30 @@ import {
 export const Route = createFileRoute("/_auth/login")({
   component: () => {
     return (
-      <>
-        <div className="flex flex-col h-screen w-full items-center justify-center">
+        <div className="bg-gray-100 flex flex-col h-screen w-full items-center justify-center">
           <div className="lg:p-8">
-            <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-              <Card >
-                <CardHeader className="items-center justify-center text-center">
-                  <img
-                    src="/logo-bea.png"
-                    alt="Logo UFAL"
-                    className="size-25"
+            <div className="flex items-center w-full justify-center">
+                <img
+                  src="/logo-bea.png"
+                  alt="Logo UFAL"
+                  className="max-w-[120px]"
                   />
-                  <CardTitle >LOGIN</CardTitle>
+                </div>
+            <div className="mt-4 mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+              <Card >
+                <CardHeader>
+                  <CardTitle>Acesse sua conta</CardTitle>
+                  <CardDescription>
+                    Insira suas credenciais para acessar o sistema
+                  </CardDescription>
                 </CardHeader>
-                <CardContent className="items-center justify-center text-center">
+                <CardContent>
                   <UserAuthForm />
                 </CardContent>
               </Card>
             </div>
           </div>
         </div>
-      </>
     );
   },
 });

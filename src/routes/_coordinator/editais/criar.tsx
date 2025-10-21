40bc3f90cf1f5c6a424983931f599042a9e2b1d3
Ruan Melo <ruantmelo@gmail.com>
2025-10-21 19:25:14 -0300

    refactor: rm unused useState import

--- src/routes/_coordinator/editais/criar.tsx
+++ src/routes/_coordinator/editais/criar.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm, type Control, type ControllerRenderProps } from "react-hook-form"
 import { z } from "zod"
